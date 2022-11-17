7eec8e082ca81cd15aa415414455536cbcdff069
neekesh <nikesthapa20@gmail.com>
2022-11-17 19:49:58 +0545

    post request

--- api/student_api.go
+++ api/student_api.go
@@ -1,16 +1,53 @@
 package api
 
-import "github.com/gin-gonic/gin"
+import (
+	"learn-go/infrastructure"
+	"learn-go/models"
+	"net/http"
 
-func GetStudent(ctx *gin.Context) {
-	ctx.JSON(200, gin.H{
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+// var DB = infrastructure.DB
+
+type StudentCreate struct {
+	FullName string `json:"full_name" binding:"required"`
+	Age      uint8  `json:"age,string" binding:"required"`
+	Address  string `json:"address" binding:"required"`
+	Major    string `json:"major" binding:"required"`
+	Grade    string `json:"grade"  binding:"required"`
+}
+
+func GetAllStudent(ctx *gin.Context) {
+
+	ctx.JSON(http.StatusOK, gin.H{
 		"msg": "get all teh student",
 	})
 }
 
-func CreateStudent(ctx *gin.Context) {
-	ctx.JSON(201, gin.H{
-		"msg": "create new student",
+func PostStudent(ctx *gin.Context) {
+	var newStudent StudentCreate
+
+	if err := ctx.BindJSON(&newStudent); err != nil {
+		log.Print("Error", err)
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	student := models.Student{
+		FullName: newStudent.FullName,
+		Age:      newStudent.Age,
+		Address:  newStudent.Address,
+		Major:    newStudent.Major,
+		Grade:    newStudent.Grade,
+	}
+
+	infrastructure.DB.Create(&student)
+	ctx.JSON(http.StatusCreated, gin.H{
+		"msg":  "new student craete",
+		"data": student,
 	})
 }
 
