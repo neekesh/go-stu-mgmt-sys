396cd6730885abba20605398588141da24dbb40a
neekesh <nikesthapa20@gmail.com>
2022-11-20 18:33:51 +0545

    fx and bootstrap imported

--- main.go
+++ main.go
@@ -1,33 +1,15 @@
 package main
 
 import (
-	"learn-go/api"
-	infrastructure "learn-go/infrastructure"
-	"learn-go/middleware"
-
-	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"go.uber.org/fx"
+	"learn-go/bootstrap"
 )
 
 func main() {
-	server := gin.Default()
 	// infrastructure.InitializeLogger()
 	// infrastructure.Logger.Info("started Server")
 	godotenv.Load()
+	fx.New(bootstrap.Module).Run()
 
-	infrastructure.ConnectDB()
-
-	infrastructure.SetupFirebase()
-
-	server.Use(middleware.FirebaseAuth)
-
-	server.GET("/", api.GetAllStudent)
-
-	server.POST("/create", api.PostStudent)
-
-	server.DELETE("/delete/:id", api.DeleteStudent)
-
-	server.PUT("/update/:id", api.UpdateStudent)
-
-	server.Run(":2000")
 }
