395ea4ba394694c7ee0a7ecbb597e6b38f935ac9
neekesh <nikesthapa20@gmail.com>
2022-11-28 16:08:47 +0545

    new student routes

---
+++ api/routes/student.go
@@ -0,0 +1,28 @@
+package routes
+
+import (
+	"learn-go/api/controllers"
+	"learn-go/infrastructure"
+)
+
+type StudentRoutes struct {
+	studentController controllers.StudentControllers
+	router            infrastructure.Router
+}
+
+func NewStudentRoutes(studentController controllers.StudentControllers, router infrastructure.Router) StudentRoutes {
+	return StudentRoutes{
+		studentController: studentController,
+		router:            router,
+	}
+}
+
+func (c StudentRoutes) Setup() {
+	student := c.router.Gin.Group("student").Use()
+	{
+		student.GET("/", c.studentController.GetAllStudent)
+		student.POST("/create", c.studentController.PostStudent)
+		student.PUT("/update", c.studentController.UpdateStudent)
+		student.DELETE("/delete", c.studentController.DeleteStudent)
+	}
+}
