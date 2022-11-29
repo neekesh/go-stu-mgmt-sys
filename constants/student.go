6ba8ad0d0a2cc888bf395f1c4e2eb28c3e272b9d
neekesh <nikesthapa20@gmail.com>
2022-11-29 11:43:12 +0545

    variables for student

---
+++ constants/student.go
@@ -0,0 +1,17 @@
+package constants
+
+type StudentCreate struct {
+	FullName string `json:"full_name" binding:"required"`
+	Age      uint8  `json:"age,string" binding:"required"`
+	Address  string `json:"address" binding:"required"`
+	Major    string `json:"major" binding:"required"`
+	Grade    string `json:"grade"  binding:"required"`
+}
+
+type UpdateStudentInput struct {
+	FullName string `json:"full_name"`
+	Age      uint8  `json:"age,string"`
+	Address  string `json:"address"`
+	Major    string `json:"major"`
+	Grade    string `json:"grade"`
+}
